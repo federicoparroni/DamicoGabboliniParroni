92bcecf3f4ab0ce92f732ba090fe05c5f581f76f
Federico Parroni <federico.parroni@mail.polimi.it>
2017-12-12 18:31:21 +0100

    Wrong namespace

--- Implementation/TravlendarApp/app/src/main/java/com/example/gabdampar/travlendar/Controller/ViewController/ScheduleCreationActivity.java
+++ Implementation/TravlendarApp/app/src/main/java/com/example/gabdampar/travlendar/Controller/ViewController/ScheduleCreationActivity.java
@@ -1,11 +1,9 @@
 package com.example.gabdampar.travlendar.Controller.ViewController;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,7 +16,7 @@ import com.example.gabdampar.travlendar.Model.ConstraintOnSchedule;
 import com.example.gabdampar.travlendar.Model.OptCriteria;
 import com.example.gabdampar.travlendar.Model.Schedule;
 import com.example.gabdampar.travlendar.R;
-import com.google.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
