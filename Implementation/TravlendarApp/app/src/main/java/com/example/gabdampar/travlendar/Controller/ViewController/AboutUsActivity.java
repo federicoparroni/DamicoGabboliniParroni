1ce7c6bcb09609880f657ce109b098abee06da4f
Federico Parroni <federico.parroni@mail.polimi.it>
2018-01-06 18:06:47 +0100

    Added About Us activity

---
+++ Implementation/TravlendarApp/app/src/main/java/com/example/gabdampar/travlendar/Controller/ViewController/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package com.example.gabdampar.travlendar.Controller.ViewController;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gabdampar.travlendar.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
