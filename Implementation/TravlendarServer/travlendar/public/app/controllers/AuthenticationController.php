1bd7d4098fe8b185faa48c4ed07b83bdfd125db8
Federico Parroni <federico.parroni@mail.polimi.it>
2018-01-06 11:36:50 +0100

    Bug fixed in registration

--- Implementation/TravlendarServer/travlendar/public/app/controllers/AuthenticationController.php
+++ Implementation/TravlendarServer/travlendar/public/app/controllers/AuthenticationController.php
@@ -35,21 +35,32 @@ class AuthenticationController
             $email = filter_var($data['email'], FILTER_SANITIZE_STRING);
             $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
 
-            $emailESC = $db->quote($email);
-            // check if user already exists
-            if($db->query("SELECT COUNT(*) FROM oauth_users WHERE username=$emailESC")->fetchColumn() == 0) {
-                // create new user in db
-                $passESC = $db->quote($password);
-                
-                $storage = new Storage\Pdo($db);
-                $storage->setUser($email, $password);
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                if(trim($password) != '') {
 
-                $statement = $db->prepare("UPDATE oauth_users SET email = :mail WHERE username = :user");
-                $statement->execute(array('mail' => $email, 'user' => $email));
-                
-                return $response->withJson(array( 'success' => true, 'message' => 'User successfully created!' ), 201);            
+                    $emailESC = $db->quote($email);
+                    // check if user already exists
+                    if($db->query("SELECT COUNT(*) FROM oauth_users WHERE username=$emailESC")->fetchColumn() == 0) {
+                        // create new user in db
+                        $passESC = $db->quote($password);
+                        
+                        $storage = new Storage\Pdo($db);
+                        $storage->setUser($email, $password);
+
+                        $statement = $db->prepare("UPDATE oauth_users SET email = :mail WHERE username = :user");
+                        $statement->execute(array('mail' => $email, 'user' => $email));
+                        
+                        return $response->withJson(array( 'success' => true, 'message' => 'User successfully created!' ), 201);            
+                    } else {
+                        return $response->withJson(array( 'success' => false, 'message' => 'User email already existing' ), 400);
+                    }
+                } else {
+                    // invalid password
+                    return $response->withJson(array( 'success' => false, 'message' => 'Incorrect password' ), 400);
+                }
             } else {
-                return $response->withJson(array( 'success' => false, 'message' => 'User email already existing' ), 400);
+                // invalid username
+                return $response->withJson(array( 'success' => false, 'message' => 'Incorrect email' ), 400);
             }
         } else {
             return $response->withJson(array( 'success' => false, 'message' => 'Incorrect email or password' ), 400);
