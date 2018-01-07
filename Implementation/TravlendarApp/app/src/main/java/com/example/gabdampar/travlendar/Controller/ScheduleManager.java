ba135db59e1c16d357994b3550aa15e7629e67f0
GiovanniGabbolini <giovanni.gabbolini@gmail.com>
2018-01-07 17:16:57 +0100

    improved directions

--- Implementation/TravlendarApp/app/src/main/java/com/example/gabdampar/travlendar/Controller/ScheduleManager.java
+++ Implementation/TravlendarApp/app/src/main/java/com/example/gabdampar/travlendar/Controller/ScheduleManager.java
@@ -37,15 +37,15 @@ public class ScheduleManager {
             return directions;
         }
         else{
+            directions += "Schedule directions: \n \n";
             for(int i=0; i<ScheduleManager.GetInstance().runningSchedule.getScheduledAppts().size(); i++) {
                 ScheduledAppointment scheduledAppointment = ScheduleManager.GetInstance().runningSchedule.getScheduledAppts().get(i);
-                directions += i==0 ? "Directions to reach " + scheduledAppointment.toString() + "\n" : "";
                 if (scheduledAppointment.dataFromPreviousToThis != null) {
                     directions += "Start at " + scheduledAppointment.dataFromPreviousToThis.getTime().startingTime.toString("HH:mm")+"\n";
                     directions += scheduledAppointment.dataFromPreviousToThis.getDirections();
                     directions += "Estimate arrival time: " + scheduledAppointment.dataFromPreviousToThis.getTime().endingTime.toString("HH:mm") + "\n \n";
                 }
-                directions += i!=0 ? scheduledAppointment.toString() + " [" + scheduledAppointment.ETA.toString("HH:mm") + " - " + scheduledAppointment.endingTime().toString("HH:mm") + "] \n" : "";
+                directions += scheduledAppointment.toString() + " [" + scheduledAppointment.ETA.toString("HH:mm") + " - " + scheduledAppointment.endingTime().toString("HH:mm") + "] \n \n";
             }
         }
         return directions;
